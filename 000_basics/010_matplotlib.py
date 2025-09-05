dd7e6e4c0f48baf57a068d4c39077d79f1478795
ClydeB <barretto.clyde@gmail.com>
2025-09-05 00:25:07 -0400

    Added Graphs + edited data

---
+++ 000_basics/010_matplotlib.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("Matplotlib Demo using ADSB Signal Data")
+# Load ADSB-like data
+file_path = 'AircraftADSBData.xlsx'
+data = pd.read_excel(file_path)
+print("Data Loaded from Excel:")
+print("Columns in DataFrame:", data.columns.tolist())
+# Display first few rows
+
+print(data.head())
+
+# Plot Information
+plt.figure(figsize=(10, 6))
+for icao in data['ICAO'].unique():
+    subset = data[data['ICAO'] == icao]
+    plt.plot(subset['Timestamp'], subset['Altitude'], label=str(icao))
+plt.title('Time Lapse and Altitude for each ICAO')
+plt.xlabel('Timestamp')
+plt.ylabel('Altitude (feet)')
+plt.legend(title='ICAO')
+plt.grid(True)
+plt.show()
+
+
+
